4737ff79dae1fec6925cebc19aa276992dde1e8f
Giorge Abdala <giorgeabdala@gmail.com>
2024-07-10 19:50:16 -0300

    add opacity na imagem hero

--- src/app/landing/Logo.tsx
+++ src/app/landing/Logo.tsx
@@ -6,10 +6,13 @@ import ButtonDefault from "@/app/components/Button";
 export default function Logo() {
     return (
         <div
-            className="bg-gradient-to-b  from-black   via-background-via-connect to-black">
+            className="bg-gradient-to-b  from-black   via-background-via-connect to-black relative">
             <div className="flex justify-center flex-1 mt-36">
                 <Image className="rounded-3xl" src="/PolkaBlockchain.png" alt="logotipo da comunidade Polkadot Brasil"
-                       width={1165} height={500}/>
+                       width={1165} height={500}
+                style={{opacity: 0.8}}
+
+                />
             </div>
         </div>
     );
