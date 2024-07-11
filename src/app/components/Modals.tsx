f316c1057e451cb7a1708bd5389d61a42ec18df8
Giorge Abdala <giorgeabdala@gmail.com>
2024-07-10 22:30:14 -0300

    gradiente no título do hunter
    border no botão de ingresso.

--- src/app/components/Modals.tsx
+++ src/app/components/Modals.tsx
@@ -13,7 +13,8 @@ export default function Modals() {
         <div>
             <ButtonDefault
                 className=" px-8 py-2 shadow-lg text-xl bg-gradient-to-r from-polkadot-rose via-pink-500 to-polkadot-rose
-                transition-transform duration-300 transform hover:scale-105"
+                transition-transform duration-300 transform hover:scale-105
+                border-2 border-transparent hover:border-white"
                 text="Garanta seu passaporte"
                 onClick={() => setTwitterModal(true)}
             />
--- src/app/landing/Hunter.tsx
+++ src/app/landing/Hunter.tsx
@@ -12,7 +12,7 @@ export default function Hunter() {
                         <Image className="rounded-3xl max-w-xs" src={blockrio} alt="logo da Blockchain Festival" />
                     </div>
                     <div className="flex flex-col items-center justify-center mt-20 lg:mt-0 lg:col-span-2">
-                        <p className="font-bold text-3xl text-center text-polkadot-rose">Caça ao Tesouro Polkadot Brasil no Blockchain Rio 2024</p>
+                        <p className="font-bold text-3xl text-center bg-gradient-to-r from-polkadot-rose via-pink-400 to-polkadot-rose bg-clip-text text-transparent">Caça ao Tesouro Polkadot Brasil no Blockchain Rio 2024</p>
                         <div className="flex flex-col justify-center text-justify leading-loose text-2xl text-zinc-100 gap-4 mt-14">
                             <p>Participe da nossa emocionante Caça ao Tesouro durante o Blockchain Rio 2024 e descubra todas as novidades do ecossistema Polkadot!</p>
                             <p>Participe da emocionante Caça ao Tesouro durante o Blockchain Rio 2024 e descubra todas as novidades do ecossistema Polkadot! Visite os estandes do universo Polkadot, colete NFTs exclusivos e concorra a uma coleção completa de NFTs físicos. Não perca essa chance de explorar, aprender e ganhar prêmios incríveis. Junte-se a nós e vivencie a experiência completa Polkadot!</p>
