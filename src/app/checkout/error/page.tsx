94949029722d08d3501e52ac971ee00e8acc233a
Giorge Abdala <giorgeabdala@gmail.com>
2024-07-14 15:59:27 -0300

    alterado pagina de sucesso para layout novo.

--- src/app/checkout/error/page.tsx
+++ src/app/checkout/error/page.tsx
@@ -5,15 +5,16 @@ import Image from 'next/image'
 
 export default function checkout() {
     return (
-        <Area className="bg-transparent h-screen">
-            <div className="flex flex-col items-center justify-center  ">
+        <Area className="bg-black relative h-screen">
+            <div className="flex flex-col items-center justify-center mt-16 gap-14">
                 {/* eslint-disable-next-line react/jsx-no-undef */}
-                <Image src="/logo_transparent.png" alt="logotipo da comunidade Polkadot Brasil" width={500} height={500} />
-                <h1 className="text-4xl font-bold  font-title text-polkadot-rose ">POLKADOT BRASIL</h1>
+                <Image className="rounded-3xl" src="/PolkaBlockchain.png" alt="logotipo da comunidade Polkadot Brasil" width={800} height={600} />
+                <h1 className="text-2xl font-bold  font-title text-polkadot-rose md:text-4xl">POLKADOT BRASIL</h1>
             </div>
-            <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-lg text-white">
+            <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-xl text-white leading-loose">
                 <div>
-                    Houve algum erro no processamento da sua solicitação. Por favor, procure os administradores da Polkadot Brasil pelo telegram ou no Discord
+                    <p> Houve algum erro no processamento da sua solicitação.</p>
+                    <p> Por favor, procure os administradores da Polkadot Brasil pelo telegram ou no Discord </p>
                     </div>
 
             </div>
--- src/app/checkout/sucess/page.tsx
+++ src/app/checkout/sucess/page.tsx
@@ -5,21 +5,20 @@ import Image from 'next/image'
 
 export default function checkout() {
     return (
-        <Area className="bg-transparent h-screen">
-            <div className="flex flex-col items-center justify-center  ">
+        <Area className="bg-gradient-to-b  from-black   via-background-via-connect to-black relative h-screen">
+            <div className="flex flex-col items-center justify-center mt-16 gap-14">
                 {/* eslint-disable-next-line react/jsx-no-undef */}
-                <Image src="/logo_transparent.png" alt="logotipo da comunidade Polkadot Brasil" width={500} height={500} />
+                <Image className="rounded-3xl" src="/PolkaBlockchain.png" alt="logotipo da comunidade Polkadot Brasil" width={800} height={600} />
                 <h1 className="text-2xl font-bold  font-title text-polkadot-rose md:text-4xl">POLKADOT BRASIL</h1>
             </div>
             <div className="flex flex-col font-body items-center font-black text-justify mt-20 gap-8 text-xl text-white leading-loose">
                 <p>PARABÉNS!!!</p>
 
-                <div className="font-normal">
-                    Já reservamos seu passaporte para a incrível Blockchain Rio! Nos próximos momentos, você receberá uma mensagem via WhatsApp, trazendo as orientações detalhadas para a retirada do seu voucher.
+                <div className="font-body leading-loose">
+                    <p>Já reservamos seu passaporte para a Blockchain Rio!</p>
+                    <p> Em breve, você receberá uma mensagem via WhatsApp com as instruções para retirar seu voucher.</p>
                     </div>
-                    <div>
-                        Se prepare para uma jornada repleta de aprendizado, networking e descobertas emocionantes. Estamos ansiosos para compartilhar essa aventura inovadora com você!
-                    </div>
+
 
             </div>
 
