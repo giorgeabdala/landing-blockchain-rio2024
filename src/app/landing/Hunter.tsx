ae958e3378443f93ef13d9a1b30d0e72fbd99cc1
Giorge Abdala <giorgeabdala@gmail.com>
2024-07-16 11:51:05 -0300

    corrigido responsividade para celular.

--- src/app/landing/Hunter.tsx
+++ src/app/landing/Hunter.tsx
@@ -6,16 +6,18 @@ import blockrio from "public/nfts/nftFisica.png";
 export default function Hunter() {
     return (
         <div className="bg-gradient-to-b from-background-primary to-background-via-connect">
-            <div className="mx-32 my-36">
-                <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center lg:justify-items-start items-center font-body">
-                    <div className="flex items-center justify-center">
-                        <Image className="rounded-3xl max-w-xs" src={blockrio} alt="logo da Blockchain Festival" />
+            <div className="mx-4 lg:mx-32 my-12 lg:my-36">
+                <div className="grid grid-cols-1 lg:grid-cols-3 justify-items-center lg:justify-items-start items-center font-body gap-8">
+                    <div className="flex items-center justify-center lg:justify-start">
+                        <Image className="rounded-3xl max-w-xs w-full" src={blockrio} alt="logo da Blockchain Festival" />
                     </div>
-                    <div className="flex flex-col items-center justify-center mt-20 lg:mt-0 lg:col-span-2">
-                        <p className="font-bold text-3xl text-center bg-gradient-to-r from-polkadot-rose via-pink-400 to-polkadot-rose bg-clip-text text-transparent">Caça ao Tesouro Polkadot Brasil no Blockchain Rio 2024</p>
-                        <div className="flex flex-col justify-center text-justify leading-loose text-2xl text-zinc-100 gap-4 mt-14">
-                            <p>Participe da nossa emocionante Caça ao Tesouro durante o Blockchain Rio 2024 e descubra todas as novidades do ecossistema Polkadot!</p>
-                            <p>Participe da emocionante Caça ao Tesouro durante o Blockchain Rio 2024 e descubra todas as novidades do ecossistema Polkadot! Visite os estandes do universo Polkadot, colete NFTs exclusivos e concorra a uma coleção completa de NFTs físicos. Não perca essa chance de explorar, aprender e ganhar prêmios incríveis. Junte-se a nós e vivencie a experiência completa Polkadot!</p>
+                    <div className="flex flex-col items-center justify-center mt-8 lg:mt-0 lg:col-span-2 px-4">
+                        <p className="font-bold text-3xl text-center bg-gradient-to-r from-polkadot-rose via-pink-400 to-polkadot-rose bg-clip-text text-transparent">
+                            Caça ao Tesouro Polkadot Brasil no Blockchain Rio 2024
+                        </p>
+                        <div className="flex flex-col justify-center text-justify leading-loose lg:text-2xl text-zinc-100 gap-4 mt-8 lg:mt-14">
+                            <p>Participe da emocionante Caça ao Tesouro durante o Blockchain Rio 2024 e descubra as novidades do ecossistema Polkadot! Visite os estandes do universo Polkadot, colete NFTs exclusivos e concorra a uma coleção completa de NFTs físicos. </p>
+                              <p>  Junte-se a nós e vivencie a experiência completa Polkadot!</p>
                         </div>
                     </div>
                 </div>
--- src/app/landing/Logo.tsx
+++ src/app/landing/Logo.tsx
@@ -7,7 +7,7 @@ export default function Logo() {
     return (
         <div
             className="bg-gradient-to-b  from-black   via-background-via-connect to-black relative">
-            <div className="flex justify-center flex-1 mt-36">
+            <div className="flex justify-center flex-1 mt-12 lg:mt-28">
                 <Image className="rounded-3xl" src="/PolkaBlockchain.png" alt="logotipo da comunidade Polkadot Brasil"
                        width={1165} height={500}
                 style={{opacity: 0.8}}
